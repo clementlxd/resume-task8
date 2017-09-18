5e1dea2268002eca9c9bbe44ab94f3d0b1f26c30
lxd <clementlxd@139.com>
2017-09-18 15:00:28 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,7 @@
 import Topbar from "./components/Topbar"
 import ResumeEditor from "./components/ResumeEditor"
 import ResumePreview from "./components/ResumePreview"
+  import icons from './assets/icons'
 
 export default {
   name: 'app',
@@ -26,33 +27,37 @@ export default {
       text: "你好"
     }
   },
-  components:{Topbar,ResumeEditor,ResumePreview}
+  components:{Topbar,ResumeEditor,ResumePreview},
+  created(){
+    document.body.insertAdjacentHTML('afterbegin',icons)
+  }
 }
 
 </script>
 
-<style>
-  .page{
-  height: 100vh;
-  display: flex;
-  flex-direction:column;
-    background: #eaebec;
-  }
-  .page>main{
-    flex-grow: 1;
-  }
-
-  .page>main{
-    min-width: 1024px;
-    max-width: 1440px;
-    margin-top: 16px;
-    margin-bottom: 16px;
+<style lang="scss">
+  .page {
+    height: 100vh;
     display: flex;
-    justify-content:space-between;
-    padding:0 16px;
-    width: 100%;
-    align-self:center;
+    flex-direction: column;
+    background: #eaebec;
 
+    >main{
+      flex-grow: 1;
+    }
+
+    >main{
+      min-width: 1024px;
+      max-width: 1440px;
+      margin-top: 16px;
+      margin-bottom: 16px;
+      display: flex;
+      justify-content: space-between;
+      padding: 0 16px;
+      width: 100%;
+      align-self: center;
+
+    }
   }
 
   #resumeEditor{
@@ -65,6 +70,14 @@ export default {
     flex-grow: 1;
     margin-left: 16px;
     background: #777;
+  }
+
+  svg.icon{
+    height: 1em;
+    width: 1em;
+    fill:currentColor;
+    vertical-align: -0.1em;
+    font-size:16px;
   }
 
 </style>
--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -2,6 +2,11 @@
   <div id="topbar">
     <div class="wrapper">
       <span class="logo">Resume</span>
+
+      <div class="actions">
+        <button class="primary">保存</button>
+        <button>预览</button>
+      </div>
     </div>
   </div>
 </template>
@@ -12,30 +17,29 @@
   }
 </script>
 
-<style>
-  #topbar{
+<style scoped lang="scss">
+  #topbar {
     background: #ffffff;
-    box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.25);
 
-  }
+    > .wrapper {
+      min-width: 1024px;
+      max-width: 1440px;
+      margin: 0 auto;
+      height: 64px;
+    }
 
-  #topbar>.wrapper{
-    min-width: 1024px;
-    max-width: 1440px;
-    margin:0 auto;
-    height: 64px;
-  }
+    > .wrapper {
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      padding: 0 16px;
+    }
 
-  #topbar>.wrapper{
-    display:flex;
-    justify-content:space-around;
-    align-items:center;
-    padding:0 16px;
-  }
-
-  #topbar .logo{
-    font-size: 24px;
-    color: #000000;
+    .logo {
+      font-size: 24px;
+      color: #000000;
+    }
   }
 </style>
 
